64163ab37189075620f3fe5747b2522550d6dbad
Zhasur <sidamatov.offwork@gmail.com>
2022-08-10 10:30:57 +0300

    Delete SceneDelegate

--- SurfSummerSchoolProject/Application/AppDelegate.swift
+++ SurfSummerSchoolProject/Application/AppDelegate.swift
@@ -10,9 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        runMainFlow()
         return true
+    }
+    
+    func runMainFlow() {
+        window?.rootViewController = TabBarConfigurator().configure()
     }
 }
 
